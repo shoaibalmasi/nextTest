303b1317953cbc7e700195637f417d0ec9ab4883
shoaib <shoaibalmasi@gmail.com>
2023-04-11 17:17:21 +0330

    login

--- pages/login.js
+++ pages/login.js
@@ -3,13 +3,15 @@ import Layout from "@/components/Layout"
 
 function LoginPage(){
 
+    return(
     <Layout title="Login">
-        <div className="flex justify-center">
+        <div className="flex justify-center items-center pt-72">
 
-            <div className="bg-white">
-                <form>
-                    <input>username</input>
-                    <input>password</input>
+            <div className="bg-indigo-300 px-10 py-20 flex justify-cnter items-center rounded-xl">
+                <form className="flex flex-col justify-between space-y-3">
+                    <input className="rounded-xl  px-4 py-2" type="text" placeholder=" username"/>
+                    <input className="rounded-xl  px-4 py-2" type="password" placeholder=" password"/>
+                    <button className="rounded-xl bg-gray-700 text-white px-4 py-2" >login</button>
                 </form>
 
             </div>
@@ -17,6 +19,8 @@ function LoginPage(){
         </div>
 
     </Layout>
+
+    )
 }
 
 export default LoginPage
